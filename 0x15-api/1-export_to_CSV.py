5efdadb1a121cab41e668278d2a09d9da395453a
Carlos Zuluaga <carlosez9@hotmail.com>
2020-02-03 23:44:48 -0500

    Fix: Change on parameters for csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Gather data from an API and export the data to a CSV file"""
+"""Gather data from an API"""
 
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     url_todo = 'https://jsonplaceholder.typicode.com/todos'
     url_employee = 'https://jsonplaceholder.typicode.com/users'
     if len(sys.argv) > 1:
@@ -20,6 +20,14 @@ if __name__ == "__main__":
             employee_data = []
 
         employee_name = employee_data[0].get('name')
+        total_tasks = len(todo_data)
+        tasks_completed = 0
+        tasks_completed_description = []
+        for task in todo_data:
+            if task.get('completed'):
+                tasks_completed += 1
+                tasks_completed_description.append(task.get('title'))
+
         csv_list = []
         task_data = []
         for task in todo_data:
