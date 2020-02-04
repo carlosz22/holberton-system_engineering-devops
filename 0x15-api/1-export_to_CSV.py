c3c46cf74344409d684f0c1a7429dd8f9808ab61
Carlos Zuluaga <carlosez9@hotmail.com>
2020-02-03 23:28:26 -0500

    Fix: Change on parameters for csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,31 +5,32 @@ import csv
 import requests
 import sys
 
-url_todo = 'https://jsonplaceholder.typicode.com/todos'
-url_employee = 'https://jsonplaceholder.typicode.com/users'
-if len(sys.argv) > 1:
-    employee_id = sys.argv[1]
-    todo_data = requests.get(url_todo, params={'userId': employee_id})
-    employee_data = requests.get(url_employee, params={'id': employee_id})
-    try:
-        todo_data = todo_data.json()
-        employee_data = employee_data.json()
-    except:
-        todo_data = []
-        employee_data = []
+if __name__ == "__main__":
+    url_todo = 'https://jsonplaceholder.typicode.com/todos'
+    url_employee = 'https://jsonplaceholder.typicode.com/users'
+    if len(sys.argv) > 1:
+        employee_id = sys.argv[1]
+        todo_data = requests.get(url_todo, params={'userId': employee_id})
+        employee_data = requests.get(url_employee, params={'id': employee_id})
+        try:
+            todo_data = todo_data.json()
+            employee_data = employee_data.json()
+        except:
+            todo_data = []
+            employee_data = []
 
-    employee_name = employee_data[0].get('name')
-    csv_list = []
-    task_data = []
-    for task in todo_data:
-        task_data = [employee_id,
-                     employee_name,
-                     str(task.get('completed')),
-                     task.get('title')]
-        csv_list.append(task_data)
+        employee_name = employee_data[0].get('name')
+        csv_list = []
+        task_data = []
+        for task in todo_data:
+            task_data = [employee_id,
+                         employee_name,
+                         str(task.get('completed')),
+                         task.get('title')]
+            csv_list.append(task_data)
 
-    with open(employee_id + '.csv', 'w', newline='') as file:
-        writer = csv.writer(file, delimiter=',', quotechar='"',
-                            quoting=csv.QUOTE_ALL)
-        for row in csv_list:
-            writer.writerow(row)
+        with open(employee_id + '.csv', 'w') as file:
+            writer = csv.writer(file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+            for row in csv_list:
+                writer.writerow(row)
