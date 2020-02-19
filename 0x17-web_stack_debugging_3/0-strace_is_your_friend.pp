4496b5deb58d59f4d80e19c48d1c85ebf0a3bfe5
Carlos Zuluaga <carlosez9@hotmail.com>
2020-02-19 11:01:52 -0500

    Add exercise 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix the config file for Apache to direct to the correct wp config file
+exec { 'fix config file':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,3 @@
+# Read me
+
+This is the readme file for 0x17-web_stack_debugging_3
