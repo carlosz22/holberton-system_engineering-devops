9b01ab4fa093cbfd279ad781a902b6509b314350
Carlos Zuluaga <carlosez9@hotmail.com>
2020-02-27 23:44:33 -0500

    Add exercise 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,18 +1,12 @@
+# Fix number of files opened in Nginx
 include stdlib
 
-class nginx {
-
-  service { 'nginx':
-    ensure => 'running',
-    enable => true,
-  }
-
-  file_line { 'u limit nginx':
-    notify => Service['nginx'],
-    ensure => present,
-    path   => '/etc/default/nginx',
-    line   => 'ULIMIT="-n 4096"',
-    match  => 'ULIMIT="-n 15"',
-  }
+file_line { 'u limit nginx':
+  ensure => present,
+  path   => '/etc/default/nginx',
+  line   => 'ULIMIT="-n 4096"',
+  match  => 'ULIMIT="-n 15"',
 }
-
+-> exec {'restart nginx':
+  command => 'service nginx restart',
+}
